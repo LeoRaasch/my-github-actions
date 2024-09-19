b9f3bd0f72012c7ebcb6be7c191144282c4a676f
Leo Raasch <raaschleo@gmail.com>
2024-09-19 17:25:21 +0200

    Added zulip-pr-reminder action

---
+++ zulip-pr-reminder/fetch-open-reviews.py
@@ -0,0 +1,38 @@
+from github import Github
+from datetime import datetime
+import json
+import pytz
+import os
+
+#Git - Zulip name mapping 
+git_to_zulip = json.loads(os.getenv('MAP'))
+
+# Set up conncetion to git repository
+git = Github(os.getenv('GITHUB_TOKEN'))
+repo = git.get_repo(os.getenv('REPO'))
+
+# Fetch open pull requests
+pulls = repo.get_pulls(state='open', sort='created')
+
+message = ""
+# Create review reminder for each pr
+for pr in pulls:
+    requested_reviewers, requested_teams = pr.get_review_requests()
+    requested_reviewers_name = [reviewer.login for reviewer in requested_reviewers]
+    requested_teams_slug = [team.slug for team in requested_teams]
+    requested_reviewers_all = requested_reviewers_name + requested_teams_slug
+    open_reviewers_zulip = [git_to_zulip.get(user, f"{user}") for user in requested_reviewers_all]
+    open_reviewers_joined = ", ".join(open_reviewers_zulip)
+
+    daysAgo = (datetime.now(pytz.utc) - pr.created_at).days
+    stalenessTime = (datetime.now(pytz.utc) - pr.updated_at).days
+
+    if open_reviewers_zulip:
+        message += f"[#{pr.number}] [{pr.title}]({pr.html_url}) ({pr.user.login})\n {stalenessTime} days stale - {daysAgo} days old - Waiting on: {open_reviewers_joined}\n"
+
+with open('/home/runner/work/_temp/message.txt', 'w') as file:
+    file.write(message.strip())
+    
+    
+
+
